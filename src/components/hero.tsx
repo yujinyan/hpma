15522f716472bbe0fdda910ed816c5cdc3e25a7d
Yu Jinyan <yujinyan@outlook.com>
2022-05-24 13:29:47 +0800

    add outfit display to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,19 @@
-import * as React from "react"
-import {StaticImage} from "gatsby-plugin-image"
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import tw from "twin.macro";
+
+const OutfitContainer = tw.div`absolute bottom-0`;
 
 
-const Hero = () => <div>
-    <StaticImage src="../images/hufflepuff_bg.jpg" alt=""/>
-</div>
+const Hero = () => <div tw="relative">
+  <StaticImage src="../images/hufflepuff_bg.jpg" alt="" />
+  <StaticImage
+    placeholder="none"
+    tw="absolute bottom-0 z-20 h-5/6 left-1/2"
+    // imgStyle={{height: "100%"}}
+    objectFit="contain"
+    objectPosition="left bottom"
+    src="../../content/assets/outfits/outfit-2.png" alt="" />
+</div>;
 
-export default Hero
+export default Hero;
