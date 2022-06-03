e4bf0298c0d4d4896e721f046cf50d694620babb
Yu Jinyan <yujinyan@outlook.com>
2022-06-03 17:15:08 +0800

    improve hero bg

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,11 +6,14 @@ const OutfitContainer = tw.div`absolute bottom-0`;
 
 
 const Hero = () => <div tw="relative">
-  <StaticImage src="../images/hufflepuff_bg.jpg" layout="fullWidth" alt="" />
+  <StaticImage
+    objectFit="cover"
+    tw="aspect-[4/3] lg:aspect-video"
+    src="../images/hufflepuff-bg-optimized.jpeg"
+    alt="" />
   <StaticImage
     placeholder="none"
     tw="absolute bottom-0 z-20 h-5/6 left-1/2"
-    // imgStyle={{height: "100%"}}
     objectFit="contain"
     objectPosition="left bottom"
     src="../../content/assets/outfits/outfit-2.png" alt="" />
