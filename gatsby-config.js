7623556b5f1f00d71f03bbca317b07c05ee3df22
Yu Jinyan <yujinyan@outlook.com>
2022-06-18 18:44:27 +0800

    update icons, theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,12 +53,12 @@ module.exports = {
                 name: `gatsby-starter-default`,
                 short_name: `starter`,
                 start_url: `/`,
-                background_color: `#663399`,
+                background_color: `#b17220`,
                 // This will impact how browsers show your PWA/website
                 // https://css-tricks.com/meta-theme-color-and-trickery/
-                // theme_color: `#663399`,
+                theme_color: `#b17220`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/hufflepuff-pride.svg`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
